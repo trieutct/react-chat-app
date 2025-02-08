9fe202d497e4519003c61dcf594a900491efb448
trieutct <trinhcongtrieu2972002@gmail.com>
2025-02-08 23:15:20 +0700

    done update profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,56 @@
 import Navbar from "./components/Navbar";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SingUpPage from "./pages/SingUpPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
+
 const App = () => {
+    const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+    useEffect(() => {
+        checkAuth();
+    }, [checkAuth]);
+
+    if (isCheckingAuth && !authUser) {
+        return (
+            <div className="flex items-center justify-center h-screen">
+                <Loader className="size-10 animate-spin" />
+            </div>
+        );
+    }
+
     return (
         <div>
             <Navbar />
             <Routes>
-                <Route paht="/" element={<HomePage />} />
-                <Route paht="/singup" element={<SingUpPage />} />
-                <Route paht="/login" element={<LoginPage />} />
-                <Route paht="/settings" element={<SettingsPage />} />
-                <Route paht="/profile" element={<ProfilePage />} />
+                <Route
+                    path="/"
+                    element={authUser ? <HomePage /> : <Navigate to="/login" />}
+                />
+                <Route
+                    path="/signup"
+                    element={!authUser ? <SingUpPage /> : <Navigate to="/" />}
+                />
+                <Route
+                    path="/login"
+                    element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+                />
+                <Route path="/settings" element={<SettingsPage />} />
+                <Route
+                    path="/profile"
+                    element={
+                        authUser ? <ProfilePage /> : <Navigate to="/login" />
+                    }
+                />
             </Routes>
+
+            <Toaster />
         </div>
     );
 };
---
+++ src/store/useAuthStore.js
@@ -0,0 +1,74 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
+
+export const useAuthStore = create((set) => ({
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+    isCheckingAuth: true,
+
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get("/auth/check");
+            set({ authUser: res.data });
+        } catch (error) {
+            set({ authUser: null });
+            console.log(`Error checkAuth: ${error}`);
+        } finally {
+            set({ isCheckingAuth: false });
+        }
+    },
+
+    singup: async (data) => {
+        set({ isSigningUp: true });
+        try {
+            const res = await axiosInstance.post("/auth/signup", data);
+            set({ authUser: res.data });
+            toast.success("Account created successfully");
+        } catch (error) {
+            console.log("error", error);
+            toast.error(error?.response?.data?.message);
+        } finally {
+            set({ isSigningUp: false });
+        }
+    },
+
+    logOut: async () => {
+        try {
+            await axiosInstance.post("auth/logout");
+        } catch (error) {
+            toast.error(error?.response?.data?.message);
+        }
+    },
+
+    login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+            const res = await axiosInstance.post("/auth/login", data);
+            set({ authUser: res.data });
+            toast.success("Logged in successfully");
+
+            // get().connectSocket();
+        } catch (error) {
+            toast.error(error?.response?.data?.message);
+        } finally {
+            set({ isLoggingIn: false });
+        }
+    },
+
+    updateProfile: async (data) => {
+        set({ isUpdatingProfile: true });
+        try {
+            const res = await axiosInstance.put("/auth/update-profile", data);
+            set({ authUser: res.data });
+            toast.success("Profile updated successfully");
+        } catch (error) {
+            console.log("error in update profile:", error);
+            toast.error(error?.response?.data?.message);
+        } finally {
+            set({ isUpdatingProfile: false });
+        }
+    },
+}));
